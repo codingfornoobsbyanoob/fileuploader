87a0b98f6890e0db13b2db702b07b33d62731aa2
Timmy <timmycryptocoin@gmail.com>
2019-04-17 02:07:26 +1000

    tidy up js and links

--- public/index.js
+++ public/index.js
@@ -1,10 +1,7 @@
-// document.getElementById('test').innerHTML = '<p>Test</p>'
 document.getElementById('uploader').elements[2].addEventListener('click', (e) => {
     // e.preventDefault()
 document.getElementById('thanks').innerHTML = '<p>Thanks for uploading</p>'
 })
 
-// const x = document.getElementById('uploader').elements[2]
-// console.log(x)
 
 
