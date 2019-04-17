2096d2256fc479bb1e6d2624e24317757bc22093
Timmy <timmycryptocoin@gmail.com>
2019-04-17 10:45:26 +1000

    try remove js confirm

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
-document.getElementById('uploader').elements[2].addEventListener('click', (e) => {
-    // e.preventDefault()
-document.getElementById('thanks').innerHTML = '<p>Thanks for uploading</p>'
-})
+// document.getElementById('uploader').elements[2].addEventListener('click', (e) => {
+//     // e.preventDefault()
+// document.getElementById('thanks').innerHTML = '<p>Thanks for uploading</p>'
+// })
 
 
 
